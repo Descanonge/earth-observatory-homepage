2e7ea4ec3e53cdd53ff6e4ab01767c42f3968dd1
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-06-21 03:25:14 +0200

    Use EarthObservatory instead
    also add cache

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Visible Earth Homepage
 
-An extension setting the homepage to the latest image from http://visibleearth.nasa.gov.
+An extension setting the homepage to the latest image from https://earthobservatory.nasa.gov.
 
 https://addons.mozilla.org/en-US/firefox/addon/visibleearthhomepage/
 
 The high-resolution image is displayed in full width.
 A small caption gives the title and links to the corresponding VisibleEarth webpage.
 
-The latest entry is found by scraping the main page (the RSS flux regularly has issues with publishing dates, and is slow to load). The large image is also found by scraping the entry page.
+The latest entry information is found with the RSS feed. The high-resolution image url is found by scraping the article page. This is saved to local storage to speed subsequent accesses as much as possible.
 
 **Disclaimer:** this is amateur code, I barely know what I am doing
 
@@ -16,4 +16,9 @@ This is not affiliated with NASA.
 ## Permissions
 
 - webRequest
-- access to `https://visibleearth.nasa.gov/*`
+- storage
+- access to `https://earthobservatory.nasa.gov/*`
+
+## Name
+
+The project and extension were initially recovering the image from the Visible Earth website (https://visibleearth.nasa.gov). It turns out it is easier to use Earth Observatory: the RSS flux is faster, and the high-resolution image is easier to scrape. Same images, different websites, I kept the same name.
--- js/index.js
+++ js/index.js
@@ -1,59 +1,38 @@
-/* Main script
 
-  Check the main VisibleEarth page to get entries.
-  I don't use the existing RSS feed. It regularly has problems in publishing dates
-  and is quite slow to load.
-*/
+// Use cache to speed things up
+browser.storage.local.get(["title", "subtitle", "url", "image_url"])
+       .then((item) => {
+         $(document).find("#ve-img").attr("src", item.image_url);
+         $(document).find("#ve-title").html(item.title);
+         $(document).find("#ve-subtitle").html(item.subtitle);
+         $(document).find("#ve-link").attr("href", item.url);
+       })
 
 $.get({
-  url: "https://visibleearth.nasa.gov/",
-  dataType: "html",
+  url: "https://earthobservatory.nasa.gov/feeds/image-of-the-day.rss",
+  dataType: "xml",
   success: function(res, status) {
-    var html = $.parseHTML(res);
-    var items = $(html).find(".image__cell").map(function(index, cell) {
-      var title = $(cell).find(".image--info");
-      return {
-        url: $(title).find("a").attr("href"),
-        title: $(title).find("a").html().trim().replace(/(\s|\n|&nbsp;)*$/, ''),
-        date: Date.parse($(title).find("p").first().html().replace(/^Published /, '')),
-        subtitle: $(title).find("p").eq(1).html().replace(/(\s|\n|&nbsp;)*$/, '')
-      }
-    }).get();
+    var item_rss = $(res).find("item").first();
+    var item = {
+      title: $(item_rss).find("title").text(),
+      subtitle: $(item_rss).find("description").text(),
+      url: $(item_rss).find("link").text()
+    }
 
-    items = items.sort((a, b) => {
-      var a = a["date"],
-          b = b["date"];
-      if (a < b) {return -1;}
-      else if (a > b) {return 1;}
-      else {return 0;}
-    }).reverse();
-
-    var item = items[0];
-    $(document).find("#ve-title").html(item["title"]);
-    $(document).find("#ve-subtitle").html(item["subtitle"]);
-    $(document).find("#ve-title").on("click", function(event) {
-      window.location.href = "https://visibleearth.nasa.gov" + item["url"];
-    });
+    $(document).find("#ve-title").html(item.title);
+    $(document).find("#ve-subtitle").html(item.subtitle);
+    $(document).find("#ve-link").attr("href", item.url);
 
     $.get({
-      url: "https://visibleearth.nasa.gov" + item["url"],
+      url: item.url,
       dataType: "html",
       success: function(res, status) {
-        var html = $.parseHTML(res);
-        var images = $(res).find(".child-image").find("img").map(function(index, img) {
-          return $(img).attr("src");
-        }).get();
-        // default to first image found
-        var url = images[0];
-        for (img of images) {
-          if (/_lrg\.jpg$/.test(img)) {
-            url = img;
-            break;
-          }
-        }
-        $(document).find("#ve-img").attr("src", url);
+        var image_url = $(res).find(".panel-footer").find("a, .downloat-btn").attr("href");
+        $(document).find("#ve-img").attr("src", image_url);
+        item.image_url = image_url;
       }
     });
 
+    browser.storage.local.set(item);
   }
 });

3ef46acfc3c6c346cca8ca04f67cc61dfc6110a5
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-08-29 17:14:23 +0200

    Add max-width and fix various
    MaxWidth to avoid zooming small images too much
    
    Fix caching of image_url
    
    Avoid downloading images when parsing xml/html

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 
 // Use cache to speed things up
-browser.storage.local.get(["title", "subtitle", "url", "image_url"])
+browser.storage.local.get(["title", "subtitle", "url", "image_url", "max_width"])
        .then((item) => {
-         $(document).find("#ve-img").attr("src", item.image_url);
-         $(document).find("#ve-title").html(item.title);
-         $(document).find("#ve-subtitle").html(item.subtitle);
-         $(document).find("#ve-link").attr("href", item.url);
+         document.getElementById("ve-img").src = item.image_url;
+         document.getElementById("ve-title").textContent = item.title;
+         document.getElementById("ve-subtitle").textContent = item.subtitle;
+         document.getElementById("ve-link").href = item.url;
+         document.getElementById("ve-img").style.maxWidth = item.max_width;
        })
+
+var img = document.getElementById("ve-img");
+// avoid too much zoom on smaller images
+img.onload = function() {
+  var max_width = `${2*img.naturalWidth}px`;
+  img.style.maxWidth = max_width;
+  browser.storage.local.set({"width": max_width});
+}
 
 $.get({
   url: "https://earthobservatory.nasa.gov/feeds/image-of-the-day.rss",
   dataType: "xml",
   success: function(res, status) {
-    var item_rss = $(res).find("item").first();
+    // Need a virtual document to avoid loading images
+    var ownerDocument = document.implementation.createHTMLDocument('virtual');
+    var item_rss = $(res, ownerDocument).find("item").first();
     var item = {
       title: $(item_rss).find("title").text(),
       subtitle: $(item_rss).find("description").text(),
@@ -20,26 +31,27 @@ $.get({
     }
     var thumbnail = $(item_rss).find("media\\:thumbnail").attr("url");
 
-    $(document).find("#ve-title").html(item.title);
-    $(document).find("#ve-subtitle").html(item.subtitle);
-    $(document).find("#ve-link").attr("href", item.url);
+    document.getElementById("ve-title").textContent = item.title;
+    document.getElementById("ve-subtitle").textContent = item.subtitle;
+    document.getElementById("ve-link").href = item.url;
+    browser.storage.local.set(item);
 
     $.get({
       url: item.url,
       dataType: "html",
       success: function(res, status) {
-        var image_url = $(res).find(".panel-footer").find("a, .download-btn").attr("href");
         if (!(image_url.slice(-4).toLowerCase() in [".jpg", "png"])) {
+        var image_url = $(res, ownerDocument).find(".panel-footer").find("a, .download-btn").attr("href");
           image_url = thumbnail;
         }
         if (typeof(image_url) === "undefined") {
-          image_url = $(res).find(".panel-image").find("img").attr("src");
+          image_url = $(res, ownerDocument).find(".panel-image").find("img").attr("src");
         }
-        $(document).find("#ve-img").attr("src", image_url);
-        item.image_url = image_url;
+
+        document.getElementById("ve-img").src = image_url
+        browser.storage.local.set({"image_url": image_url});
       }
     });
 
-    browser.storage.local.set(item);
   }
 });

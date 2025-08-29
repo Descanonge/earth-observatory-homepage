60792b13d09aaffe8052c327cc785471f0854c1a
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-08-29 17:20:10 +0200

    Add zoom and scroll-with-drag

--- js/index.js
+++ js/index.js
@@ -63,3 +63,79 @@ $.get({
 
   }
 });
+
+// Zoom
+var zoom_level = 100;
+var cursor = "zoom-in";
+var current_scroll = window.scrollY;
+
+function resetZoom() {
+  img.style.width = "100%";
+  img.style.maxWidth = `${2*img.naturalWidth}px`;
+  window.scroll({top: current_scroll});
+}
+
+function setZoom(event) {
+  current_scroll = window.scrollY;
+  img.style.width = `${zoom_level}%`;
+  img.style.maxWidth = "";
+  window.scroll({
+    top: Math.max(0, event.offsetY * zoom_level / 100 - window.innerHeight/2),
+    left: Math.max(0, event.offsetX * zoom_level / 100 - window.innerWidth/2),
+  });
+}
+
+resetZoom();
+
+// Grab to scroll
+let pos = {top: 0, left: 0, x: 0, y: 0};
+
+const mouseClick = function(event) {
+  if (zoom_level > 100) {
+    zoom_level = 100;
+    cursor = "zoom-in";
+    resetZoom()
+  } else {
+    // zoom at "natural" size of image, with minimum zoom of 150% for small images
+    zoom_level = Math.max(150, Math.round(img.naturalWidth / window.innerWidth * 100));
+    cursor = "zoom-out";
+    setZoom(event);
+  }
+  img.style.cursor = cursor;
+}
+
+const mouseMoveHandler = function (event) {
+  const dx = event.clientX - pos.x;
+  const dy = event.clientY - pos.y;
+
+  window.scroll({top: pos.top - dy, left: pos.left - dx});
+  document.removeEventListener("mouseup", mouseClick);
+};
+
+const mouseUpHandler = function (event) {
+  document.removeEventListener("mouseup", mouseClick);
+  document.removeEventListener("mousemove", mouseMoveHandler);
+  document.removeEventListener("mouseup", mouseUpHandler);
+
+  img.style.cursor = cursor;
+  img.style.removeProperty("user-select");
+}
+
+const mouseDownHandler = function (event) {
+  pos = {
+    left: window.scrollX,
+    top: window.scrollY,
+    x: event.clientX,
+    y: event.clientY
+  };
+
+  document.addEventListener("mouseup", mouseClick);
+  document.addEventListener("mousemove", mouseMoveHandler);
+  document.addEventListener("mouseup", mouseUpHandler);
+  event.preventDefault()
+
+  img.style.cursor = "grabbing";
+  img.style.userSelect = "none";
+};
+
+img.addEventListener("mousedown", mouseDownHandler);
